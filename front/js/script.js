0cd317a1ea7b626f8a5a37ff1f6ef24569fafb1b
DanDWD <rubic@live.fr>
2022-11-17 21:55:05 +0100

    Simplification du code script.js

--- front/js/script.js
+++ front/js/script.js
@@ -3,33 +3,23 @@ fetch("http://localhost:3000/api/products")
 
 .then((response) => response.json())
 
-  //recuperation du resultat sous forme de table (>log)
-  .then((articlesList) => {   
-    console.table(articlesList);
-    display(articlesList);
+  //recuperation du resultat
+  .then((data) => {   
+    console.log(data);
+    displayAllProducts(data);
   })
   //en cas d'erreur
   .catch((err) => { 
-    document.querySelector(".items").innerHTML = "<h2>Erreur 404. L'API est injoinable</h2>";
-    console.log("L'API est injoinable" + err);
+    document.querySelector(".items").innerHTML = `
+    <h2>Erreur de connection</h2>
+    <p>${err}</p>
+    `
   });
 
-
 //----affichage des articles sur la page index
-function display(articlesList) {
-let newHtml = "";
-
-articlesList.forEach(article => { 
-  newHtml += displayItems(article)
-});
-
-//selection de la zone d'affichage
-document.getElementById("items").innerHTML = newHtml;
-}
-
-//----code à injecter dans le html
-function displayItems(article) {
-  return  `
+function displayAllProducts(data) {
+  data.forEach(article => { 
+  document.getElementById("items").innerHTML +=  `
     <a href="./product.html?id=${article._id}">
       <article>
         <img src="${article.imageUrl}" alt="${article.altTxt}">
@@ -37,4 +27,4 @@ function displayItems(article) {
         <p class="productDescription">${article.description}</p>
       </article>
     </a>`;
-}
+})};
